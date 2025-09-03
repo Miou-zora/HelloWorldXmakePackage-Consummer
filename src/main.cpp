bcffaa95d7b43f9814067444e2a56926d1a3ad5a
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2025-09-03 10:52:59 +0200

    Consumme headeronly package

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
 #include <foo.h>
+#include <bar.h>
 
 int main(int argc, char** argv) {
     std::cout << "Value of foo:" << std::to_string(foo()) << std::endl;
+    std::cout << "Value of bar:" << std::to_string(bar) << std::endl;
     return 0;
 }
